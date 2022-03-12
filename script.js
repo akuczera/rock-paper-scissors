fb2c7665f7307fb6040795cef01c0e84d4045363
Alex <kuczera.alexander@gmail.com>
2022-03-12 12:10:25 -0700

    Built out HTML template with links to script.js and style.css Wrote down pseudocode for first two function in Javascript.

---
+++ script.js
@@ -0,0 +1,11 @@
+//start with function computerPlay that randomly returns either Rock,
+//Paper, or Scissors. This function will make the "computers play"
+
+//create a function to play s single round of RPS, should take two
+//parameters, playerSelection & computerSelection, returns a string that
+//declares the winner of the game (ie. You lose! The computer wins!).
+//NOTE: playerSelection should be case insensitive, doesn't matter how
+//someone types it in. & "RETURN" the results of this function, not
+//console.log() it, because we want to use this data later.
+
+//START WITH THESE TWO FUNCTIONS THEN MOVE ONTO PART 5 OF THE PROJECT.
