fd1030d41594efd579cd69815d66677b93b4ff87
akuczera <97765892+akuczera@users.noreply.github.com>
2022-03-17 23:05:01 +0000

    Created playerSelection function to prompt user to pick Rock, Paper, Scissors.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 Paper, or Scissors. This function will make the "computers play" */
 
 //Create array of string options
+
 var options = [
   'rock',
   'paper',
@@ -10,7 +11,7 @@ var options = [
 //Declare the function
 function computerPlay() {
   let randomPick = options[Math.floor(Math.random() * options.length)];
-  return randomPick();
+  return randomPick;
 }
 
 /* Create a function to play a single round of RPS, should take two
@@ -20,14 +21,14 @@ NOTE: playerSelection should be case insensitive, doesn't matter how
 someone types it in. & "RETURN" the results of this function, not
 console.log() it, because we want to use this data later. */
 
-//Declare what playerSelection variable is.
-  //Ask player to type in selection.
-  //Change the case sensitivity to have all lowercase letters. Then you
-  //can "match them later".
-  //Put this choice into playerSelection variable
-//Declare what computerSelection is.
-  //This comes from the computerPlay() function.
-  //Put computerPlay output into computerSelection variable.
+let playerSelection = window.prompt("What do you choose? Rock, Paper, or Scissors?");
+playerSelection = playerSelection.toLowerCase();
+
+let computerSelection = computerPlay();
+
+
+
+
 //Create function with two parameters from above.
   //Same choice is a tie.
   //Rock beats scissors.
