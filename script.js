ccda9d81dec31c69f3d602c625b9b6a81d073a83
akuczera <97765892+akuczera@users.noreply.github.com>
2022-03-18 02:02:27 +0000

    Created function playRound() that will play one round of RPS, and declare the winner.

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-/* Start with function computerPlay that randomly returns either Rock,
-Paper, or Scissors. This function will make the "computers play" */
-
-//Create array of string options
-
+//Created array of options for 'computer' to select.
 var options = [
   'rock',
   'paper',
   'scissors'
 ]
-//Declare the function
+
+//Function that creates an integer between 0-2 and then returns corresponding option from array.
 function computerPlay() {
   let randomPick = options[Math.floor(Math.random() * options.length)];
   return randomPick;
 }
 
-/* Create a function to play a single round of RPS, should take two
-parameters, playerSelection & computerSelection, returns a string that
-declares the winner of the game (ie. You lose! The computer wins!).
-NOTE: playerSelection should be case insensitive, doesn't matter how
-someone types it in. & "RETURN" the results of this function, not
-console.log() it, because we want to use this data later. */
-
+//Asking 'player' to type in a choice.
 let playerSelection = window.prompt("What do you choose? Rock, Paper, or Scissors?");
 playerSelection = playerSelection.toLowerCase();
 
+//Assigning computerPlay() function to new variable.
 let computerSelection = computerPlay();
 
+//Creates one round of game, decides who the winner is based on string comparison.
+function playRound(param1, param2) {
+  if ((param1 === "rock") && (param2 === "scissors")) {
+    alert("Computer wins! Rock crushes scissors."); 
+  } else if ((param1 === "scissors") && (param2 === "rock")) {
+    alert("You win! Rock crushes scissors");
+  } else if ((param1 === "scissors") && (param2 === "paper")) {
+    alert("Computer wins! Scissors cut through paper.");
+  } else if ((param1 === "paper") && (param2 === "scissors")) {
+    alert("You win! Scissors cut through paper.");
+  } else if ((param1 === "paper") && (param2 === "rock")) {
+    alert("Computer wins! Paper covers rock.");
+  } else if ((param1 === "rock") && (param2 === "paper")) {
+    alert("You win! Paper covers rock.");
+  } else {
+    alert("It's a tie!");
+  }
+}
 
-
-
-//Create function with two parameters from above.
-  //Same choice is a tie.
-  //Rock beats scissors.
-  //Paper beats rock.
-  //Scissors beats paper.
-  //Output the "winner" with a certain string on each option above.
-
-//START WITH THESE TWO FUNCTIONS THEN MOVE ONTO PART 5 OF THE PROJECT.
+//Run the game with variables we created earlier.
+playRound(computerSelection, playerSelection);
