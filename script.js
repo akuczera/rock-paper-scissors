af9ee1ee137e13669af6eadef47c558eb875b4a0
Alex <kuczera.alexander@gmail.com>
2022-04-19 18:28:01 -0600

    Added UI to rock, paper, scissors

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+let randompick;
+let computerCount = 0;
+let playerCount = 0;
+let playerSelection;
+let computerSelection;
+
 //Created array of options for 'computer' to select.
 const options = [
   'rock',
@@ -11,63 +17,65 @@ function computerPlay() {
   randomPick = options[Math.floor(Math.random() * options.length)];
   return randomPick;
 }
+ 
+//buttons to return the text of button clicked.
+const btn = document.querySelectorAll('button');
 
-function playerChoice() {
-  let input = window.prompt("What do you choose? Rock, Paper, or Scissors?").toLowerCase();
-  
-  if ( input == "rock" || input == "paper" || input == "scissors") {
-    selected = input;
-  } else {
-    playerChoice();
-  }
-  return selected;
-}
-  
-let randompick;
-let computerCount = 0;
-let playerCount = 0;
-let playerSelection;
-let computerSelection;
+btn.forEach(btn => btn.addEventListener('click', () => {
+  playerSelection = btn.innerText.toLowerCase();
+  computerSelection = computerPlay();
+  playRound(computerSelection, playerSelection);
+}));
 
 function playRound(param1, param2) {
+  
+  var element = document.getElementById("score");
+
   if ((param1 === "rock") && (param2 === "scissors")) {
     computerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else if ((param1 === "scissors") && (param2 === "rock")) {
     playerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else if ((param1 === "scissors") && (param2 === "paper")) {
     computerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else if ((param1 === "paper") && (param2 === "scissors")) {
     playerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else if ((param1 === "paper") && (param2 === "rock")) {
     computerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. Computer wins this round.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else if ((param1 === "rock") && (param2 === "paper")) {
     playerCount++;
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. You win this round!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   } else {
-    alert(`Computer chose ${computerSelection}, you chose ${playerSelection}. This round is a tie.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`)
+    var text = `Computer chose ${computerSelection}, you chose ${playerSelection}. This round is a tie.\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`;
   }
+
+  element.innerText = text;
+
+  if (computerCount == 5) {
+    var para = document.createElement("p");
+    var final = document.createTextNode("The computer has won this time! Choose again to start over.");
+    element.appendChild(para);
+    para.appendChild(final);
+    computerCount = 0;
+    playerCount = 0;
+    return;
+   } else if (playerCount == 5) {
+    var para = document.createElement("p");
+    var final = document.createTextNode("You win the game! Choose again to start over.");
+    element.appendChild(para);
+    para.appendChild(final);
+    computerCount = 0;
+    playerCount = 0;
+    return;
+ }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    computerSelection = computerPlay();
-    playerSelection = playerChoice();
-    
-    playRound(computerSelection, playerSelection);
-  }
 
-  if (computerCount > playerCount) {
-    alert(`The computer has won this time!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`);
-  } else if (playerCount > computerCount) {
-    alert(`You win the game!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`);
-  } else {
-    alert(`The game is a tie!\n\nComputer: ${computerCount}\nPlayer: ${playerCount}`);
-  }
-}
 
-game();
+
+
+
